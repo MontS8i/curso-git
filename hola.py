b3b3511f97f44bdd1c67c30a6cdf95c93d662987
MontS8i <79656534+MontS8i@users.noreply.github.com>
2021-09-09 21:17:02 -0500

    Revert "Update hola.py"
    This reverts commit d3de1e8b164bf043026830dacc12d2093efb6b28, reversing
    changes made to 4bf36aeb0396cb6d5f8a81eb3f1fe933756431db.

--- hola.py
+++ hola.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     print("Ejecutao cuando se invoca directamente")
 else:
     print("Ejecutado al ser importado")
-<<<<<<< HEAD
     
     
     
@@ -14,7 +13,3 @@ else:
     
     
     print("Ola k ase")
-=======
-
-    print("ya pude jaja")
->>>>>>> f53ce7aa3bb8143f5b1cc7120273145b7ec3773a
